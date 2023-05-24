48cf5e7b43e01cfd06d9226e86f5068ceaae3ce5
Marcos Vinicius <128155251+marcos74mv@users.noreply.github.com>
2023-05-24 14:45:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,4 @@ def count_vowels(string: str) -> int:
             count += 1
 
     return count
-    
-print(count_vowels("xyz"))       # Saída: 0
-print(count_vowels("aeiou"))     # Saída: 5
-print(count_vowels("AbCdeFG"))   # Saída: 3
-print(count_vowels("hello123"))  # Saída: 2
-print(count_vowels("hello!@#"))  # Saída: 2
+   
